e90ce9c07399903688d72b9cb76d8a0bd2fea56e
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-10-22 00:17:33 +0300

    fix context issue

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,59 +1,59 @@
 import {
-    createContext,
-    useContext,
-    useEffect,
-    useState,
-    type Dispatch,
-    type SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type Dispatch,
+  type SetStateAction,
 } from "react";
 import type { IUserData } from "@/types";
 import { getProfile } from "@/api/profile/profile";
 
 type UserContextType = {
-    user: IUserData | null;
-    handleGetUser: () => void;
-    setUser: Dispatch<SetStateAction<IUserData | null>>;
+  user: IUserData | null;
+  handleGetUser: () => void;
+  setUser: Dispatch<SetStateAction<IUserData | null>>;
 };
 
 export const UserContext = createContext<UserContextType>({
-    user: null,
-    handleGetUser: () => {},
-    setUser: () => {},
+  user: null,
+  handleGetUser: () => {},
+  setUser: () => {},
 });
 
 export const UserContextProvider = ({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) => {
-    const [user, setUser] = useState<IUserData | null>(null);
+  const [user, setUser] = useState<IUserData | null>(null);
 
-    const handleGetUser = async () => {
-        try {
-            const res = await getProfile();
-            setUser(res);
-        } catch (error) {
-            console.error("Get user error:", error);
-        }
-    };
+  const handleGetUser = async () => {
+    try {
+      const res = await getProfile();
+      setUser(res);
+    } catch (error) {
+      console.error("Get user error:", error);
+    }
+  };
 
-    useEffect(() => {
-        handleGetUser();
-    }, []);
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      handleGetUser();
+    }
+  }, []);
 
-    return (
-        <UserContext.Provider value={{ user, handleGetUser, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={{ user, handleGetUser, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUserContext = () => {
-    const context = useContext(UserContext);
-    if (!context) {
-        throw new Error(
-            "useUserContext must be used within a UserContextProvider"
-        );
-    }
-    return context;
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error("useUserContext must be used within a UserContextProvider");
+  }
+  return context;
 };
